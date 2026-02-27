0cb1c3d2104690e97c1cec14e1cb140a6175dde6
Ricardo Salveti <ricardo.salveti@oss.qualcomm.com>
2026-02-27 16:35:41 -0300

    Add support to generate output URL
    Add fileserver_url as input and URL as output to allow clients to
    configure the address used for the HTTP S3 bucket interface.
    
    Fixes #19.
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@oss.qualcomm.com>

--- publish_artifacts.sh
+++ publish_artifacts.sh
@@ -10,3 +10,6 @@ fi
 # Step 2: Upload artifacts
 aws s3 cp "$INPUT_PATH" "s3://$INPUT_S3_BUCKET/$INPUT_DESTINATION" --recursive --progress-frequency 30
 
+# Step 3: Publish fileserver URL containing the artifacts
+output_file="${GITHUB_OUTPUT}"
+echo "url=${INPUT_FILESERVER_URL}/${INPUT_S3_BUCKET}/${INPUT_DESTINATION}/" >> ${output_file}
